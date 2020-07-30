a9768e45140afce04abec4ab2e60d4dbd6f6364c
kobecurry17 <670744887@qq.com>
2020-07-31 00:01:44 +0800

    feat:深拷贝一下结构

---
+++ Base/src/main/java/algorithmvariant/DeepClone.java
@@ -0,0 +1,36 @@
+package algorithmvariant;
+
+import javafx.scene.chart.ValueAxis;
+import lombok.Data;
+
+import java.util.HashMap;
+
+/**
+ * 深拷贝以下结构
+ * public static  class Node{
+ * int value;
+ * Node next; 下一个结点
+ * Node rand; 随机结点
+ * }
+ * publci static
+ */
+public class DeepClone {
+
+    @Data
+    public static class Node {
+        int value;
+        Node next;
+        Node rand;
+    }
+
+    public Node clone1(Node head) {
+        HashMap<>
+        while (head.next != null) {
+
+        }
+
+
+        return new Node();
+    }
+
+}
