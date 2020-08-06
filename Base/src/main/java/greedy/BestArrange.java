9242510d7cd8d7e4dedfe5743b4a79eb8a23e345
zengyi <yi.zeng@ly.com>
2020-08-06 21:40:56 +0800

    feat:最佳排班

---
+++ Base/src/main/java/greedy/BestArrange.java
@@ -0,0 +1,117 @@
+package greedy;
+
+import java.util.Arrays;
+
+/**
+ * 一些项目要占用一个会议室宣讲，会议室不能同时容纳两个项目的宣讲
+ * 给你每一个项目的开始时间和结束的时间
+ * 你来安排宣讲的日程，要求会议室进行的宣讲场次最多
+ * 返回最多的宣讲场次
+ */
+public class BestArrange {
+    /**
+     * 宣讲课实体
+     */
+    public static class Class implements Comparable<Class> {
+        Integer start;
+        Integer end;
+
+        public Class(Integer start, Integer end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Class o) {
+            return end.compareTo(o.end);
+        }
+    }
+
+
+    static Class[] generateClass(int size) {
+        Class[] classes = new Class[size];
+        int start;
+        int end;
+        for (int i = 0; i < size; i++) {
+            start = (int) (Math.random() * 23);
+            end = (int) ((24 - start) * (Math.random()));
+            classes[i] = new Class(start, end);
+        }
+        return classes;
+    }
+
+    public static void main(String[] args) {
+        int loops = 50_0000;
+        int maxSize = 5;
+
+        for (int i = 0; i < loops; i++) {
+            Class[] classes = generateClass((int) ((Math.random() * maxSize) + 1));
+            if (maxClass1(classes) != maxClass2(classes)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("Nice");
+
+    }
+
+    private static int maxClass2(Class[] classes) {
+        return process(classes, 0, 0);
+    }
+
+    /**
+     * classes 还剩下的会议
+     * done 已经进行的会议个数
+     * timeline 时间线，已经到达的时间
+     *
+     * @param classes
+     * @param done
+     * @param timeLine
+     * @return
+     */
+    private static int process(Class[] classes, int done, int timeLine) {
+        if (null == classes || classes.length == 0) {
+            return done;
+        }
+        Class[] cls;
+        int max = done;
+        for (int i = 0; i < classes.length; i++) {
+            if (classes[i].end >= timeLine) {
+                cls = copyArrayButExcept(classes, i);
+                max = Math.max(max, process(cls, done + 1, timeLine));
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 生成新数组，并剔除不符合条件的元素
+     *
+     * @param classes
+     * @param index
+     * @return
+     */
+    private static Class[] copyArrayButExcept(Class[] classes, int index) {
+        Class[] cls = new Class[classes.length - 1];
+        int k = 0;
+        for (int i = 0; i < classes.length; i++) {
+            if (i != index) {
+                cls[k++] = classes[i];
+            }
+        }
+        return cls;
+    }
+
+    private static int maxClass1(Class[] classes) {
+        int max = 0;
+        int timeLine = 0;
+
+
+
+        Arrays.sort(classes);
+
+
+        return max;
+    }
+
+
+}
