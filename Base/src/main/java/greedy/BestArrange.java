d709b731d0db07304cf6eff199457526a96c77da
kobecurry17 <670744887@qq.com>
2020-08-09 21:39:54 +0800

    feat:安排最多场宣讲

--- Base/src/main/java/greedy/BestArrange.java
+++ Base/src/main/java/greedy/BestArrange.java
@@ -1,6 +1,8 @@
 package greedy;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * 一些项目要占用一个会议室宣讲，会议室不能同时容纳两个项目的宣讲
@@ -27,6 +29,21 @@ public class BestArrange {
         }
     }
 
+    public static class MyComparable implements Comparator<Class> {
+
+
+        @Override
+        public int compare(Class o1, Class o2) {
+            if (null == o1) {
+                return -1;
+            }
+            if (null == o2) {
+                return 1;
+            }
+            return o1.end.compareTo(o2.end);
+        }
+    }
+
 
     static Class[] generateClass(int size) {
         Class[] classes = new Class[size];
@@ -34,7 +51,7 @@ public class BestArrange {
         int end;
         for (int i = 0; i < size; i++) {
             start = (int) (Math.random() * 23);
-            end = (int) ((24 - start) * (Math.random()));
+            end = start + 1 + (int) ((24 - start) * (Math.random()));
             classes[i] = new Class(start, end);
         }
         return classes;
@@ -42,7 +59,7 @@ public class BestArrange {
 
     public static void main(String[] args) {
         int loops = 50_0000;
-        int maxSize = 5;
+        int maxSize = 50;
 
         for (int i = 0; i < loops; i++) {
             Class[] classes = generateClass((int) ((Math.random() * maxSize) + 1));
@@ -75,9 +92,9 @@ public class BestArrange {
         Class[] cls;
         int max = done;
         for (int i = 0; i < classes.length; i++) {
-            if (classes[i].end >= timeLine) {
+            if (classes[i].start >= timeLine) {
                 cls = copyArrayButExcept(classes, i);
-                max = Math.max(max, process(cls, done + 1, timeLine));
+                max = Math.max(max, process(cls, done + 1, classes[i].end));
             }
         }
         return max;
@@ -101,15 +118,28 @@ public class BestArrange {
         return cls;
     }
 
+    /**
+     * 最多能安排多少场宣讲
+     *
+     * @param classes
+     * @return
+     */
     private static int maxClass1(Class[] classes) {
         int max = 0;
         int timeLine = 0;
-
-
-
-        Arrays.sort(classes);
-
-
+        MyComparable myComparable = new MyComparable();
+        PriorityQueue<Class> queue = new PriorityQueue(myComparable);
+        for (Class c : classes) {
+            queue.add(c);
+        }
+        while (timeLine<24&&(!queue.isEmpty()))
+        {
+            Class poll = queue.poll();
+            if(poll.start>=timeLine){
+                timeLine = poll.end;
+                max++;
+            }
+        }
         return max;
     }
 
