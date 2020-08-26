d875ed3cb443ebedb6018f7080bdc52d89a249f6
kobecurry17 <670744887@qq.com>
2020-08-26 21:55:17 +0800

    feat:机器人走路问题

---
+++ Base/src/main/java/trymodel/RobotWalk.java
@@ -0,0 +1,24 @@
+package trymodel;
+
+/**
+ * 机器人走路
+ * 假设有排成一行的N个位置，记为 1~N,N一定大于或等于2
+ * 开始时机器人在其中的M的位置上（M一定是1~N的其中一个）
+ * 如果机器人来到 1 的位置下一步一定只能向右来到 2 的位置
+ * 如果机器人来到 N 的位置下一步一定只能向左来到 N-1 的位置
+ * 如果机器人来到 中间 的位置下一步能向左也能向右
+ * 规定机器人必须走 K 步，最终能来到P的位置（P也是1~N的其中一个）的方法一共有多少种
+ * 给定四个参数 M,K,N,P返回方法数
+ */
+public class RobotWalk {
+
+    public static int walkWays(int M, int K, int N, int P) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
