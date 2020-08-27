a8e4ddb58d9faabcd2a34fc7ccaa44eed659c057
zengyi <yi.zeng@ly.com>
2020-08-27 20:17:35 +0800

    feat:机器人走路

--- Base/src/main/java/trymodel/RobotWalk.java
+++ Base/src/main/java/trymodel/RobotWalk.java
@@ -2,21 +2,176 @@ package trymodel;
 
 /**
  * 机器人走路
- * 假设有排成一行的N个位置，记为 1~N,N一定大于或等于2
- * 开始时机器人在其中的M的位置上（M一定是1~N的其中一个）
+ * 假设有排成一行的SIZE个位置，记为 1~SIZE,SIZE一定大于或等于2
+ * 开始时机器人在其中的FROM的位置上（FROM一定是1~SIZE的其中一个）
  * 如果机器人来到 1 的位置下一步一定只能向右来到 2 的位置
- * 如果机器人来到 N 的位置下一步一定只能向左来到 N-1 的位置
+ * 如果机器人来到 SIZE 的位置下一步一定只能向左来到 SIZE-1 的位置
  * 如果机器人来到 中间 的位置下一步能向左也能向右
- * 规定机器人必须走 K 步，最终能来到P的位置（P也是1~N的其中一个）的方法一共有多少种
- * 给定四个参数 M,K,N,P返回方法数
+ * 规定机器人必须走 STEP 步，最终能来到TO的位置（TO也是1~SIZE的其中一个）的方法一共有多少种
+ * 给定四个参数 FROM,STEP,SIZE,TO返回方法数
  */
 public class RobotWalk {
-
-    public static int walkWays(int M, int K, int N, int P) {
-        return 0;
+    /**
+     * 递归方法
+     *
+     * @param from
+     * @param step
+     * @param size
+     * @param to
+     * @return
+     */
+    public static int walkWays1(int from, int step, int size, int to) {
+        // 起点和终点间隔大于SIZE，到不了
+        if (Math.abs(to - from) > size || size < 2) {
+            return 0;
+        }
+        return process(from, step, size, to);
     }
 
+    /**
+     * 记忆化搜索
+     *
+     * @return
+     */
+    public static int walkWays2(int from, int step, int size, int to) {
+        // 起点和终点间隔大于SIZE，到不了
+        if (Math.abs(to - from) > size || size < 2) {
+            return 0;
+        }
+        int[][] arr = new int[size + 1][step + 1];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = -1;
+            }
+        }
+        return process1(from, step, size, to, arr
+        );
+    }
+
+    /**
+     * 记忆化搜索优化
+     *
+     * @param from
+     * @param step
+     * @param size
+     * @param to
+     * @param arr
+     * @return
+     */
+    private static int process1(int from, int step, int size, int to, int[][] arr) {
+        if (step == 0) {
+            if (from != to) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        int res = 0;
+        if (from == 1) {
+            if (arr[2][step - 1] != -1) {
+                return arr[2][step - 1];
+            }
+            int i = process1(2, step - 1, size, to, arr);
+            arr[2][step - 1] = i;
+            return i;
+        }
+        if (from == size) {
+            int i = process1(size - 1, step - 1, size, to, arr);
+            arr[size - 1][step - 1] = i;
+            return i;
+        }
+        res += process1(from + 1, step - 1, size, to, arr);
+        res += process1(from - 1, step - 1, size, to, arr);
+        arr[from][step] = res;
+        return res;
+    }
+
+    /**
+     * 从from出发走step步到to有多少种方法
+     *
+     * @param from
+     * @param step
+     * @param size
+     * @param to
+     * @return
+     */
+    private static int process(int from, int step, int size, int to) {
+        if (step == 0) {
+            if (from != to) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        int res = 0;
+        if (from == 1) {
+            return process(2, step - 1, size, to);
+        }
+        if (from == size) {
+            return process(size - 1, step - 1, size, to);
+        }
+        res += process(from + 1, step - 1, size, to);
+        res += process(from - 1, step - 1, size, to);
+        return res;
+    }
+
+    /**
+     * 动态规划
+     *
+     * @param from
+     * @param step
+     * @param size
+     * @param to
+     * @return
+     */
+    private static int dpWays(int from, int step, int size, int to) {
+        // 起点和终点间隔大于SIZE，到不了
+        if (Math.abs(to - from) > size || size < 2) {
+            return 0;
+        }
+        int[][] dp = new int[size + 1][step + 1];
+
+        // dp[*][0] = 0
+        // dp[to][0] = 1
+        dp[to][0] = 1;
+        for (int step1 = 1; step1 < step + 1; step1++) {
+            for (int from1 = 1; from1 < size; from1++) {
+                if (from1 == 1) {
+                    dp[from1][step1] = dp[2][step1 - 1];
+                    continue;
+                }
+                if (from1 == size) {
+                    dp[from1][step] = dp[from1 - 1][step - 1];
+                    continue;
+                }
+                dp[from1][step1] += dp[from + 1][step1 - 1];
+                dp[from1][step1] += dp[from - 1][step1 - 1];
+            }
+        }
+        return dp[from][step];
+    }
+
+    // for testing
+    public static int generate(int max) {
+        return (int) (Math.random() * max) + 1;
+    }
+
+
     public static void main(String[] args) {
+
+        int loops = 50_0000;
+        int maxSize = 5;
+        int maxStep = 20;
+        for (int i = 0; i < loops; i++) {
+            int size = generate(maxSize);
+            int step = generate(maxStep);
+            int from = generate(size);
+            int to = generate(size);
+            if (walkWays2(from, step, size, to) != dpWays(from, step, size, to)) {
+                System.out.println("Oops!");
+            }
+            System.out.println(1);
+        }
 
     }
 
