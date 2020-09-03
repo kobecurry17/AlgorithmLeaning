db3318e9f465b160bae64e7f65b485a4387b5bda
zengyi <yi.zeng@ly.com>
2020-09-03 18:53:33 +0800

    feat：达标子数组数量

---
+++ Common/src/main/java/window/ReachTheStandard.java
@@ -0,0 +1,22 @@
+package window;
+
+/**
+ * 给定一个整型数组arr和一个整数num
+ * 某个arr中的子数组sub,如果想达标，必须满足：
+ * sub中最大值-sub中最小值<=num
+ * 返回arr中达标子数组数量
+ */
+public class ReachTheStandard {
+
+    /**
+     * 达标子数组数量
+     *
+     * @param arr
+     * @param num
+     * @return
+     */
+    public static int ans1(int arr, int num) {
+        return 0;
+    }
+
+}
