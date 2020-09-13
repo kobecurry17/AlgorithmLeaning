693d5f3b6c6c86f2e5321bd8e9f77c86e9eadcc1
Vintage <670744887@qq.com>
2020-09-13 13:36:05 +0800

    2020-09-12 LeetCode比赛

--- Competition/src/main/java/c_200912/leaves.java
+++ Competition/src/main/java/c_200912/leaves.java
@@ -2,39 +2,47 @@ package c_200912;
 
 import java.util.Arrays;
 
+/**
+ * TODO:暂时没有理解答案，日后再说
+ * 给定一个数组有r和y组成且数组长度大于3，问：经过最少多少次调整，能让数组左侧和右侧都是r中间是y？
+ * <p>
+ * 示例 1：
+ * 输入：leaves = "rrryyyrryyyrr"
+ * 输出：2
+ * 解释：调整两次，将中间的两片红叶替换成黄叶，得到 "rrryyyyyyyyrr"
+ * <p>
+ * 示例 2：
+ * 输入：leaves = "ryr"
+ * 输出：0
+ * 解释：已符合要求，不需要额外操作
+ * <p>
+ * 提示：
+ * 3 <= leaves.length <= 10^5
+ * leaves 中只包含字符 'r' 和字符 'y'
+ */
+@SuppressWarnings("all")
 public class leaves {
 
-    public static int ans1(String s) {
-        if (null == s || s.length() < 3) {
-            return -1;
+    public static int ans1(String leaves) {
+        int n = leaves.length();
+        char[] array = leaves.toCharArray();
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < n; i++)
+            sum[i + 1] = sum[i] + (array[i] == 'r' ? 1 : 0);
+        int[] min = new int[n + 1];
+        int currentMin = Integer.MAX_VALUE;
+        for (int i = 1; i < n - 1; i++) {
+            currentMin = Math.min(currentMin, i - 2 * sum[i]);
+            min[i] = currentMin;
         }
-        char[] arr = s.toCharArray();
-        int[] yArr = new int[arr.length];
-        int[] rArr = new int[arr.length];
-        if (arr[0] == 'y') {
-            yArr[0] = 1;
-            rArr[0] = 0;
-        } else {
-            yArr[0] = 0;
-            rArr[0] = 1;
-        }
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == 'y') {
-                yArr[i] = yArr[i - 1] + 1;
-                rArr[i] = rArr[i - 1];
-            } else {
-                yArr[i] = yArr[i - 1];
-                rArr[i] = rArr[i - 1] + 1;
-            }
-        }
+        int result = Integer.MAX_VALUE;
+        for (int j = n - 1; j > 1; j--)
+            result = Math.min(result, n - sum[n] + min[j - 1] - j + 2 * sum[j]);
+        return result;
+    }
 
-        int ans = Integer.MAX_VALUE;
-        for (int L = 1; L < arr.length; L++) {
-            for (int R = L + 1; R < arr.length; R++) {
-                ans = Math.min(L-yArr[L - 1] + arr.length-R-(yArr[arr.length - 1] - yArr[R]) + rArr[R - 1] - rArr[L - 1], ans);
-            }
-        }
-        return ans;
+    public static int ans2(String leaves) {
+        return 0;
     }
 
 
@@ -63,7 +71,7 @@ public class leaves {
 
     // for test
     public static String generate(int size) {
-        int s = (int) (Math.random() * size);
+        int s = (int) (Math.random() * size) + 3;
         char[] chars = new char[s];
         for (int i = 0; i < chars.length; i++) {
             chars[i] = Math.random() > 0.5 ? 'y' : 'r';
@@ -75,8 +83,7 @@ public class leaves {
         int loops = 50_0000;
         int max = 5;
         for (int i = 0; i < loops; i++) {
-//            String s = generate(max);
-            String s = "ryy";
+            String s = generate(max);
             int i1 = minimumOperations(s);
             int i2 = ans1(s);
             if (i1 != i2) {
