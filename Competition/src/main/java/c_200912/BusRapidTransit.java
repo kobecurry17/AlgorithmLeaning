6ff8d51a2e3c7d5aacee4db1c0691842b87ee42e
Vintage <670744887@qq.com>
2020-09-13 14:54:09 +0800

    2020-09-12 LeetCode比赛

---
+++ Competition/src/main/java/c_200912/BusRapidTransit.java
@@ -0,0 +1,129 @@
+package c_200912;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * 小扣打算去秋日市集，由于游客较多，小扣的移动速度受到了人流影响：
+ * <p>
+ * 小扣从 x 号站点移动至 x + 1 号站点需要花费的时间为 inc；
+ * 小扣从 x 号站点移动至 x - 1 号站点需要花费的时间为 dec。
+ * 现有 m 辆公交车，编号为 0 到 m-1。小扣也可以通过搭乘编号为 i 的公交车，从 x 号站点移动至 jump[i]*x 号站点，耗时仅为 cost[i]。小扣可以搭乘任意编号的公交车且搭乘公交次数不限。
+ * <p>
+ * 假定小扣起始站点记作 0，秋日市集站点记作 target，请返回小扣抵达秋日市集最少需要花费多少时间。由于数字较大，最终答案需要对 1000000007 (1e9 + 7) 取模。
+ * 注意：小扣可在移动过程中到达编号大于 target 的站点。
+ * <p>
+ * 示例 1：
+ * 输入：target = 31, inc = 5, dec = 3, jump = [6], cost = [10]
+ * 输出：33
+ * <p>
+ * 解释：
+ * 小扣步行至 1 号站点，花费时间为 5；
+ * 小扣从 1 号站台搭乘 0 号公交至 6 * 1 = 6 站台，花费时间为 10；
+ * 小扣从 6 号站台步行至 5 号站台，花费时间为 3；
+ * 小扣从 5 号站台搭乘 0 号公交至 6 * 5 = 30 站台，花费时间为 10；
+ * 小扣从 30 号站台步行至 31 号站台，花费时间为 5；
+ * 最终小扣花费总时间为 33。
+ * <p>
+ * 示例 2：
+ * 输入：target = 612, inc = 4, dec = 5, jump = [3,6,8,11,5,10,4], cost = [4,7,6,3,7,6,4]
+ * 输出：26
+ * <p>
+ * 解释：
+ * 小扣步行至 1 号站点，花费时间为 4；
+ * 小扣从 1 号站台搭乘 0 号公交至 3 * 1 = 3 站台，花费时间为 4；
+ * 小扣从 3 号站台搭乘 3 号公交至 11 * 3 = 33 站台，花费时间为 3；
+ * 小扣从 33 号站台步行至 34 站台，花费时间为 4；
+ * 小扣从 34 号站台搭乘 0 号公交至 3 * 34 = 102 站台，花费时间为 4；
+ * 小扣从 102 号站台搭乘 1 号公交至 6 * 102 = 612 站台，花费时间为 7；
+ * 最终小扣花费总时间为 26。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= target <= 10^9
+ * 1 <= jump.length, cost.length <= 10
+ * 2 <= jump[i] <= 10^6
+ * 1 <= inc, dec, cost[i] <= 10^6
+ */
+public class BusRapidTransit {
+    /**
+     * 暴力递归，超时了
+     * @param target
+     * @param inc
+     * @param dec
+     * @param jump
+     * @param cost
+     * @return
+     */
+    public static int busRapidTransit(int target, int inc, int dec, int[] jump, int[] cost) {
+        HashMap<Info, Long> cache = new HashMap<>();
+        int worth = target * inc;
+        long ans = Math.min(process(target, inc, dec, jump, cost, worth, 0, 0l, cache), worth);
+        return (int) ans % (int) (1e9 + 7);
+    }
+
+    private static long process(int target, int inc, int dec, int[] jump, int[] cost, int worth, int now, long timeLine, HashMap<Info, Long> cache) {
+        if (now == target) {
+            return timeLine;
+        }
+        if (timeLine > worth) {
+            return Integer.MAX_VALUE;
+        }
+        Info key = new Info(now, timeLine);
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+        long i2 = Long.MAX_VALUE;
+        if (now != 0 && now < target) {
+            for (int i = 0; i < jump.length; i++) {
+                i2 = Math.min(i2, process(target, inc, dec, jump, cost, worth, now * jump[i], timeLine + cost[i], cache));
+            }
+        }
+        long i1 = Long.MAX_VALUE;
+        if (now > target) {
+            i1 = Math.min(i1, process(target, inc, dec, jump, cost, worth, now - 1, timeLine + dec, cache));
+        } else if (now <= 0) {
+            i1 = Math.min(i1, process(target, inc, dec, jump, cost, worth, now + 1, timeLine + inc, cache));
+        } else {
+            i1 = Math.min(process(target, inc, dec, jump, cost, worth, now - 1, timeLine + dec, cache), process(target, inc, dec, jump, cost, worth, now + 1, timeLine + inc, cache));
+        }
+        Long ans = Math.min(i1, i2);
+        cache.put(key, ans);
+        return ans;
+    }
+
+
+
+
+
+    public static class Info {
+        private int now;
+        private long timeLine;
+
+        public Info(int now, long timeLine) {
+            this.now = now;
+            this.timeLine = timeLine;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Info info = (Info) o;
+            return now == info.now &&
+                    timeLine == info.timeLine;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(now, timeLine);
+        }
+    }
+
+    public static void main(String[] args) {
+        int target = 612, inc = 4, dec = 5;
+        int[] jump = new int[]{3, 6, 8, 11, 5, 10, 4}, cost = new int[]{4, 7, 6, 3, 7, 6, 4};
+        System.out.println(busRapidTransit(target, inc, dec, jump, cost));
+    }
+
+}
