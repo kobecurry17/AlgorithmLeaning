3877ccf2d0ff05176a4a856f7d4c4476948b478a
zengyi <yi.zeng@ly.com>
2020-10-18 11:03:59 +0800

    容器装水问题

--- Traiinning/src/main/java/class3/WaterContainer.java
+++ Traiinning/src/main/java/class3/WaterContainer.java
@@ -5,7 +5,7 @@ package class3;
  * 给定一个数组arr,已知其中所有的值都是非负的，将这个数组看做一个容器，请返回容器能装多少水
  * 比如，arr = {3, 1, 2, 5, 2, 4}
  * 根据滑出的直方图就是容器形状，该容器可以装下5格水
- * 再比如,arr = {4, 5, 3, 2},该容器可以装下2格水
+ * 再比如,arr = {4, 5, 1, 3, 2},该容器可以装下2格水
  * </p>
  */
 public class WaterContainer {
@@ -26,7 +26,7 @@ public class WaterContainer {
         while (L < R) {
             for (int i = L + 1; i <= R; i++) {
                 if (arr[i] >= arr[L] || arr[i] >= arr[R]) {
-                    ans += addWater( arr, L, i);
+                    ans += addWater(arr, L, i);
                     L = i;
                 }
             }
@@ -34,7 +34,7 @@ public class WaterContainer {
         return ans;
     }
 
-    private static int addWater( int[] arr, int L, int R) {
+    private static int addWater(int[] arr, int L, int R) {
         int top = Math.min(arr[L], arr[R]);
         int ans = 0;
         for (int i = L + 1; i < R; i++) {
@@ -46,6 +46,10 @@ public class WaterContainer {
 
     public static void main(String[] args) {
         int[] arr = new int[]{3, 4, 5, 2, 1, 2, 5, 7};
+        System.out.println(waterSize(arr));
+        arr = new int[]{3, 1, 2, 5, 2, 4};
+        System.out.println(waterSize(arr));
+        arr = new int[]{4, 5, 1, 3, 2};
         System.out.println(waterSize(arr));
     }
 }
