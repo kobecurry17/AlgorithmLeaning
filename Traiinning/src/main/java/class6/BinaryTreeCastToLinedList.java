f0a1af0f14cdc7e3f81f65e44d9abf286db39d44
zengyi <yi.zeng@ly.com>
2020-10-24 15:10:33 +0800

    * 双向链表结点结构和二叉树节点结构是一样的  * 如果你把last认为是left,next认为是right  * 给定一个搜索二叉树的头结点head,请转化成一条有序的双向链表  * 并返回链表的头结点

--- Traiinning/src/main/java/class6/BinaryTreeCastToLinedList.java
+++ Traiinning/src/main/java/class6/BinaryTreeCastToLinedList.java
@@ -1,15 +1,39 @@
 package class6;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * <p>
  * 双向链表结点结构和二叉树节点结构是一样的
  * 如果你把last认为是left,next认为是right
  * 给定一个搜索二叉树的头结点head,请转化成一条有序的双向链表
  * 并返回链表的头结点
- * TODO:有啥用？
+ * TODO:看视频复习！
  * </p>
  */
-public class LinedListCastBinaryTree {
+public class BinaryTreeCastToLinedList {
+
+
+    public static ListNode convert(TreeNode head) {
+        if (head == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        add(queue, head);
+
+
+        return null;
+    }
+
+    private static void add(Queue<TreeNode> queue, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        queue.add(node);
+        add(queue, node.left);
+        add(queue, node.right);
+    }
 
 
     public static class ListNode {
@@ -31,14 +55,7 @@ public class LinedListCastBinaryTree {
             this.value = value;
         }
     }
+
+
 }
 
-//    public static TreeNode cast(ListNode head) {
-//        if (null == head) {
-//            return null;
-//        }
-//        TreeNode ans = new TreeNode(head.value);
-//        TreeNode cur = ans;
-//        TreeNode pre = ans;
-//
-//    }
