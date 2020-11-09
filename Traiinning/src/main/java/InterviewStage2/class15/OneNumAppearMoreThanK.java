d259abb890f7517b7bb76f448731863e7be72beb
zengyi <yi.zeng@ly.com>
2020-11-09 18:15:38 +0800

    给定一个数组arr,如果某个数出现次数超过了数组长度的一半,打印这个数,输入没有不打印

---
+++ Traiinning/src/main/java/InterviewStage2/class15/OneNumAppearMoreThanK.java
@@ -0,0 +1,22 @@
+package InterviewStage2.class15;
+
+/**
+ * 给定一个数组arr,如果某个数出现次数超过了数组长度的一半,打印这个数,输入没有不打印
+ * <p>
+ * 给定一个数组arr和整数k，arr长度为N,如果有某些数出现次数超过了N/K
+ * 打印这些数,如果没有不打印
+ */
+public class OneNumAppearMoreThanK {
+
+
+    public static int appearMoreThanHalf(int[] arr) {
+        int head = 0;
+        int tail = arr.length - 1;
+
+        while (head >= arr.length || tail >= 0) {
+
+        }
+    }
+
+
+}
