773c6ed382f351fb0fce2be77bfe74a8e2e53eac
zengyi <yi.zeng@ly.com>
2020-11-12 17:51:31 +0800

    *     给定一个 N * 3 的矩阵matrix，对于每一个长度为3的小数组arr,都表示一个大楼的三个数据。  *     arr[0] 表示大楼的左边界,arr[1]表示大楼的右边界,arr[2]表示大楼的高度(一定大于0)  *     大楼的都在

---
+++ Traiinning/src/main/java/InterviewStage2/class16/BuildingOutline.java
@@ -0,0 +1,11 @@
+package InterviewStage2.class16;
+
+/**
+ * <p>
+ *     给定一个 N * 3 的矩阵matrix，对于每一个长度为3的小数组arr,都表示一个大楼的三个数据。
+ *     arr[0] 表示大楼的左边界,arr[1]表示大楼的右边界,arr[2]表示大楼的高度(一定大于0)
+ *     大楼的都在
+ * </p>
+ */
+public class BuildingOutline {
+}
